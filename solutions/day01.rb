b7fc4102ebe891c6466a2bb448bf463c2ad58f90
Damy van Valenberg <damyvv@hotmail.com>
2021-12-01 12:22:52 +0100

    Improved day01 solution

--- solutions/day01.rb
+++ solutions/day01.rb
@@ -1,3 +1,3 @@
 input = File.read("day01_input.txt").lines.map(&:to_i)
 p input.each_cons(2).count {|a,b| b > a }
-p input.each_cons(3).map {|l| l.reduce(:+) }.each_cons(2).count {|a,b| b > a }
+p input.each_cons(3).map(&:sum).each_cons(2).count {|a,b| b > a }
