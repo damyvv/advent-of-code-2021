fedd71f669b12341ef95cb1e9bdb65685c3a3307
Damy van Valenberg <damyvv@hotmail.com>
2021-12-10 18:40:19 +0100

    Simplified day10 part 2

--- solutions/day10.rb
+++ solutions/day10.rb
@@ -8,17 +8,6 @@ p input.map! { |l|
     l
 }.inject(0) {|s,l| s + score[l[/(\)|\]|}|>)/]] }
 
-m = { '{' => '}', '<' => '>', '(' => ')', '[' => ']' }
-score = { ')' => 1, ']' => 2, '}' => 3, '>' => 4 }
-res = input.filter {|l| l[/(\)|\]|}|>)/] == nil }.map { |l|
-    s = 0
-    while l.length > 0
-        s = s*5 + score[m[l[-1]]]
-        l += m[l[-1]]
-        while ((n = l.gsub(/(\(\)|\[\]|{}|<>)/, '')).length != l.length)
-            l = n
-        end
-    end
-    s
-}
+score = { '(' => 1, '[' => 2, '{' => 3, '<' => 4 }
+res = input.filter {|l| l[/(\)|\]|}|>)/] == nil }.map { |l| l.chars.reverse.inject(0) {|s,c| s*5+score[c] } }
 p res.sort[res.length / 2]
